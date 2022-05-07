b10fea68e704561236352abde6c01b94f465073d
Takumi Hashimoto <takumi.hashimoto@example.com>
2022-05-07 19:02:22 +0900

    bookmark seeder now uses api response.

--- database/seeders/BookmarkSeeder.php
+++ database/seeders/BookmarkSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+use App\Libs\SelfUtil;
 use DB;
 
 class BookmarkSeeder extends Seeder
@@ -15,34 +20,56 @@ class BookmarkSeeder extends Seeder
      */
     public function run()
     {
-        $this->command->info("Bookmarks seeding started.");
+        try {
+            $selfUtil = new SelfUtil();
 
-       $bookmarkSplFileObject = new \SplFileObject(__DIR__ . '/seeddata/bookmarks.csv');
-        $bookmarkSplFileObject->setFlags(
-            \SplFileObject::READ_CSV |
-            \SplFileObject::READ_AHEAD |
-            \SplFileObject::SKIP_EMPTY |
-            \SplFileObject::DROP_NEW_LINE
-        );
+            $news_technology = $selfUtil->get_bingnews_api_by_category('ScienceAndTechnology');
 
-        $count = 0;
-        foreach ($bookmarkSplFileObject as $key => $row) {
-            if ($key === 0) {
+        } catch (RequestException $e) {
+            echo Psr7\Message::toString($e->getRequest());
+            if ($e->hasResponse()) {
+                echo Psr7\Message::toString($e->getResponse());
+            }
+        }
+
+        foreach ($news_technology as $item) {
+            if ($item === 0) {
                 continue;
             }
 
             DB::table('bookmarks')->insert([
-                'author' => trim($row[0]),
-                'description' => trim($row[1]),
-                'publishedAt' => trim($row[2]),
-                'source' => trim($row[3]),
-                'title' => trim($row[4]),
-                'url' => trim($row[5]),
-                'urlToImage' => trim($row[6]),
+                'url' => $item->url,
+                'title' => $item->title,
+                'urlToImage' => $item->urlToImage,
+                'author' => NULL,
+                'description' => $item->description,
+                'source' => $item->source,
+                'publishedAt' => $item->publishedAt,
             ]);
-            $count++;
         }
 
-        $this->command->info("Bookmark seeding finished.");
+        // $bookmarkSplFileObject = new \SplFileObject(__DIR__ . '/seeddata/bookmarks.csv');
+        // $bookmarkSplFileObject->setFlags(
+        //     \SplFileObject::READ_CSV |
+        //     \SplFileObject::READ_AHEAD |
+        //     \SplFileObject::SKIP_EMPTY |
+        //     \SplFileObject::DROP_NEW_LINE
+        // );
+
+        // foreach ($bookmarkSplFileObject as $key => $row) {
+        //     if ($key === 0) {
+        //         continue;
+        //     }
+
+        //     DB::table('bookmarks')->insert([
+        //         'author' => trim($row[0]),
+        //         'description' => trim($row[1]),
+        //         'publishedAt' => trim($row[2]),
+        //         'source' => trim($row[3]),
+        //         'title' => trim($row[4]),
+        //         'url' => trim($row[5]),
+        //         'urlToImage' => trim($row[6]),
+        //     ]);
+        // }
     }
 }
